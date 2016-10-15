7858cc2ff8a4c1d6f54512194c0fd9facc9bce52
Kevin Ellis <ellisk@mit.edu>
2016-10-15 15:06:17 -0400

    better handling of grammar for rules

--- phonology/rule.py
+++ phonology/rule.py
@@ -1,4 +1,3 @@
-from Specification import *
 from sketchSyntax import define, FunctionCall
 from features import FeatureBank
 
@@ -6,116 +5,92 @@ from features import FeatureBank
 from random import random
 import re
 
+class Specification():
+    def __init__(self): pass
+    
+    @staticmethod
+    def parse(bank, output, variable):
+        try:
+            return FeatureMatrix.parse(bank, output, variable)
+        except:
+            return ConstantPhoneme.parse(bank, output, variable)
+        
+class ConstantPhoneme(Specification):
+    def __init__(self, p): self.p = p
+    def __str__(self): return self.p
+
+    @staticmethod
+    def parse(bank, output, variable):
+        pattern = " %s = new ConstantPhoneme\(phoneme=phoneme_([0-9]+)_" % str(variable)
+        m = re.search(pattern, output)
+        if not m: raise Exception('Failure parsing ConstantPhoneme %s, pattern = %s'%(variable,pattern))
+        return ConstantPhoneme(bank.phonemes[int(m.group(1))])
+    
+class FeatureMatrix():
+    def __init__(self, featuresAndPolarities): self.featuresAndPolarities = featuresAndPolarities
+    def __str__(self):
+        elements = [ ('+' if polarity else '-')+f for polarity,f in self.featuresAndPolarities ]
+        return "[ %s ]" % (" ".join(elements))
+
+    @staticmethod
+    def parse(bank, output, variable):
+        pattern = " %s = new Vector\(mask={([01,]+)}, preference={([01,]+)}"%variable
+        m = re.search(pattern, output)
+        if not m: raise Exception('Failure parsing Vector %s'%variable)
+        preference = [ int(x) for x in m.group(2).split(",") ]
+        mask = [ int(x) for x in m.group(1).split(",") ]
+        fs = [ (preference[f] == 1, bank.features[f]) for f in range(len(bank.features)) if mask[f] ]
+        return FeatureMatrix(fs)
+
+class Guard():
+    def __init__(self, side, endOfString, specification):
+        self.side = side
+        self.endOfString = endOfString
+        self.specification = specification
+    def __str__(self):
+        parts = []
+        if self.endOfString: parts = ['#']
+        if self.specification: parts.append(str(self.specification))
+        if self.side == 'R': parts.reverse()
+        return " ".join(parts)
+
+    @staticmethod
+    def parse(bank, output, variable, side):
+        pattern = " %s = new Guard\(endOfString=([01]), spec=([a-zA-Z0-9_]+)"%variable
+        m = re.search(pattern, output)
+        if not m: raise Exception('Could not parse guard %s using pattern %s'%(variable,pattern))
+
+        endOfString = m.group(1) == '1'
+        spec = None if m.group(2) == 'null' else Specification.parse(bank, output, m.group(2))
+        return Guard(side, endOfString, spec)
+
 class Rule():
     def __init__(self, focus = [], structuralChange = [], leftTriggers = [], rightTriggers = []):
         self.focus = focus
         self.structuralChange = structuralChange
         self.leftTriggers = leftTriggers
         self.rightTriggers = rightTriggers
-
-        # Remove unimportant guard
-        while len(leftTriggers) > 0 and leftTriggers[0].alwaysTrue():
-            leftTriggers = leftTriggers[1:]
-        self.relevantLeftTriggers = leftTriggers
-        while len(rightTriggers) > 0 and rightTriggers[-1].alwaysTrue():
-            rightTriggers = rightTriggers[:-1]
-        self.relevantRightTriggers = rightTriggers
     
-    def applyRule(self, input):
-        output = []
-
-        for i in range(len(input)):
-            if self.matchesContext(input, i):
-                output.append(self.change(input[i]))
-            else:
-                output.append(input[i])
-        return output
-
-    def matchesContext(self, input, i):
-        if i < len(self.relevantLeftTriggers): return False
-        if i >= len(input) - len(self.relevantRightTriggers): return False
-        if not matchesTemplate(input[i], self.focus): return False
-
-        numberLeft = len(self.relevantLeftTriggers)
-        for i_,l_ in zip(input[(i-numberLeft):i], self.relevantLeftTriggers):
-            if not matchesTemplate(i_,l_): return False
-
-        numberRight = len(self.relevantRightTriggers)
-        for i_,r_ in zip(input[(i+1):(i+1+numberRight)], self.relevantRightTriggers):
-            if not matchesTemplate(i_,r_): return False
-        
-        return True
-    
-    def change(self, input):
-        newPhoneme = [ f for f in input ]
-        for polarity,f in self.structuralChange:
-            if polarity: newPhoneme.append(f)
-            elif f in newPhoneme: newPhoneme.remove(f)
-        return newPhoneme
-
-    def descriptionLength(self):
-        return len(self.focus) + len(self.structuralChange) + len([f for l in self.leftTriggers for f in l ]) + len([f for l in self.rightTriggers for f in l ])
-
-    def mutate(self):
-        return Rule(mutateMatrix(self.focus),
-                    mutateMatrix(self.structuralChange),
-                    map(mutateMatrix,self.leftTriggers),
-                    map(mutateMatrix,self.rightTriggers))
-
     def __str__(self):
         return "%s ---> %s / %s _ %s" % (str(self.focus),
                                          str(self.structuralChange),
-                                         " ".join(map(str,self.leftTriggers)),
-                                         " ".join(map(str,self.rightTriggers)))
-
+                                         str(self.leftTriggers),
+                                         str(self.rightTriggers))
+                                         
     # Produces sketch object
     @staticmethod
     def sample():
         return define("Rule", FunctionCall("unknown_rule",[]))
+
     # Produces a rule object from a sketch output
     @staticmethod
     def parse(bank, output, variable):
-        def decodeStructure(preference,mask):
-            return [ (preference[f] == 1,bank.features[f]) for f in range(len(bank.features)) if mask[f] == 1]
-        structures = {}
-        variable = str(variable)
-        for specification in ['focus','structural_change','left_trigger','right_trigger']:
-            specificationPattern = 'Rule.*%s.* = new Rule.*%s=(new Specification\(mask={[01,]+}, preference={[01,]+}\))' % (variable, specification)
-            endingPattern = 'Rule.*%s.* = new Rule.*%s=new EndOfString' % (variable, specification)
-            constantPattern = 'Rule.*%s.* = new Rule.*%s=new ConstantPhoneme\(([^)]+)\)' % (variable, specification)
-            m = re.search(specificationPattern, output)
-            if m:
-                s = Specification.parse(bank, m.group(1))
-            else:
-                m = re.search(endingPattern, output)
-                if m:
-                    s = EndOfString()
-                else:
-                    m = re.search(constantPattern, output)
-                    if m:
-                        s = ConstantPhoneme.parse(bank, m.group(1))
-                    else:
-                        print "Could not find the following pattern:"
-                        print output
-                        return None
-            structures[specification] = s
-        return Rule(structures['focus'],
-                    structures['structural_change'],
-                    [structures['left_trigger']],
-                    [structures['right_trigger']])
-
-
-def mutateMatrix(m):
-    # On average we add 1/4
-    expectedChanges = 0.25
-    additionProbability = expectedChanges / len(FEATURESET)
-    removalProbability = expectedChanges / (len(m) + 0.001)
-
-    return set([f for f in m if random() > removalProbability ] +
-               [(random() > 0.5, f) for f in FEATURESET if random() < additionProbability ])
-
-def matchesTemplate(candidate, template):
-    for polarity,f in template:
-        if polarity and (not (f in candidate)): return False
-        if not polarity and (f in candidate): return False
-    return True
+        pattern = 'Rule.*%s.* = new Rule\(focus=([a-zA-Z0-9_]+), structural_change=([a-zA-Z0-9_]+), left_trigger=([a-zA-Z0-9_]+), right_trigger=([a-zA-Z0-9_]+)\)' % str(variable)
+        m = re.search(pattern, output)
+        if not m:
+            raise Exception('Failure parsing rule')
+        focus = Specification.parse(bank, output, m.group(1))
+        structuralChange = Specification.parse(bank, output, m.group(2))
+        leftTrigger = Guard.parse(bank, output, m.group(3), 'L')
+        rightTrigger = Guard.parse(bank, output, m.group(4), 'R')
+        return Rule(focus, structuralChange, leftTrigger, rightTrigger)
