cd134865a9f1389e88f9b3476a2db52d3d4d8cac
Kevin M Ellis <ellisk@sketch2.csail.mit.edu>
2017-01-27 16:22:15 -0500

    Unicode headers for a few files probably doesn't need it though

--- phonology/sketch.py
+++ phonology/sketch.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from features import FeatureBank
 from sketchSyntax import *
 from rule import Rule
--- phonology/utilities.py
+++ phonology/utilities.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 def compose(f,g):
     return lambda x: f(g(x))
