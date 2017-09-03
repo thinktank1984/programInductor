b96acb89eecf5d4060a8639cff0c9c766e72a2b4
Kevin Ellis <ellisk@mit.edu>
2017-09-03 19:41:52 -0400

    debugging output

--- phonology/matrix.py
+++ phonology/matrix.py
@@ -503,6 +503,8 @@ class UnderlyingProblem():
                 print " [?] WARNING: Missing morphology for some inflections, padding with empty"
                 solution.prefixes += [Morph([])]*(len(self.data[0]) - len(solution.prefixes))
                 solution.suffixes += [Morph([])]*(len(self.data[0]) - len(solution.prefixes))
+                print solution.prefixes
+                print solution.suffixes
                 print solution
                 assert len(solution.prefixes) == len(solution.suffixes)
             elif len(solution.prefixes) > len(self.data[0]):
