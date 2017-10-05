e9ec630c1e4d79ceb5cc1902909a999476f9c046
Kevin Ellis <ellisk@mit.edu>
2017-10-05 19:41:19 -0400

    tiny bug fix in dead code

--- phonology/matrix.py
+++ phonology/matrix.py
@@ -66,7 +66,7 @@ class UnderlyingProblem():
         _r = r.makeDefinition(self.bank)
         condition(wordEqual(result,applyRule(_r,u.makeConstant(self.bank), self.maximumObservationLength + 1)))
         try:
-            output = self.solveSketch(self.bank, self.maximumObservationLength, self.maximumObservationLength)
+            output = self.solveSketch(self.bank)
         except SynthesisFailure:
             print "applyRuleUsingSketch: UNSATISFIABLE for %s %s"%(u,r)
             assert False
