40ec9bf9401e8ced46ddb4dfe38efbe223ea9821
Kevin Ellis <ellisk@mit.edu>
2018-02-27 10:50:59 -0500

    fixed a bug in the curriculum

--- phonology/curriculum.py
+++ phonology/curriculum.py
@@ -6,6 +6,6 @@ for j in range(10):
         ug = ""
     else:
         ug = "--universal universalGrammars/empirical_%d.p"%j
-    os.system("python driver.py %d incremental --top 100 %s --pickleDirectory frontierPickles/"%ug)
+    os.system("python driver.py %s incremental --top 100 %s --pickleDirectory frontierPickles/"%(j,ug))
     print "Reestimating a universal grammar..."
     os.system("pypy UG.py fromFrontiers --problems %d --export universalGrammars/empirical_%d.p"%(j+1, j+1))
