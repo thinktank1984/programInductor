df035014fa18ea980fece0ea949179b28997ca49
Kevin Ellis <ellisk@mit.edu>
2018-02-28 06:58:07 -0500

    debugging for curriculum learner

--- phonology/curriculum.py
+++ phonology/curriculum.py
@@ -6,6 +6,16 @@ for j in range(10):
         ug = ""
     else:
         ug = "--universal universalGrammars/empirical_%d.p"%j
-    os.system("python driver.py %s incremental --top 100 %s --pickleDirectory frontierPickles/"%(j,ug))
-    print "Reestimating a universal grammar..."
-    os.system("pypy UG.py fromFrontiers --problems %d --export universalGrammars/empirical_%d.p"%(j+1, j+1))
+    command = "python driver.py %s incremental --top 100 %s --pickleDirectory frontierPickles/"%(j,ug)
+    print 
+    print "\tCURRICULUM: Solving the next problem by issuing the command:"
+    print "\t\t",command
+    print 
+    os.system(command)
+
+    command = "pypy UG.py fromFrontiers --problems %d --export universalGrammars/empirical_%d.p"%(j+1, j+1)
+    print 
+    print "\tCURRICULUM: Reestimating a universal grammar by issuing the command:"
+    print "\t\t",command
+    os.system(command)
+    print 
